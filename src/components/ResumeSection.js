7aba95fb1970c49add838a92e9920166ff4f520d
PatrikParkkonen <a1704538@myy.haaga-helia.fi>
2025-02-18 00:17:05 +0200

    updating resume

--- src/components/ResumeSection.js
+++ src/components/ResumeSection.js
@@ -6,8 +6,8 @@ function ResumeSection() {
     return (
         <div className='resume-container'>
             
-            <h2><a href='/CV_Patrik_Parkkonen_2025_FINNISH.docx' download>Click here to download my resume in Finnish</a></h2>
-            <h2><a href='/CV_Patrik_Parkkonen_2025_ENGLISH.docx' download>Click here to download my resume in English</a></h2>
+            <h2><a href='/CV_Patrik_Parkkonen_2025_FINNISH.pdf' download>Click here to download my resume in Finnish</a></h2>
+            <h2><a href='/CV_Patrik_Parkkonen_2025_ENGLISH.pdf' download>Click here to download my resume in English</a></h2>
                 
             </div>
         
