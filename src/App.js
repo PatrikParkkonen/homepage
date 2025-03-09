4f27aa3db11d0ed0c672de39cdf7c16e57bc7737
PatrikParkkonen <a1704538@myy.haaga-helia.fi>
2025-03-09 20:53:28 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import Aboutme from './components/pages/Aboutme';
-import Resume from './components/pages/Resume';
 import Games from './components/pages/Games';
 import Wordle from './components/pages/Wordle';
 import Connections from './components/pages/Connections';
@@ -22,7 +21,6 @@ function App() {
       <Routes>
         <Route path='/' exact element={<Home />} />
         <Route path='/aboutme' element={<Aboutme />} />
-        <Route path='/resume' element={<Resume />} />
         <Route path='/games' element={<Games />} />
         <Route path='/wordle' element={<Wordle />} />
         <Route path='/connections' element={<Connections />} />
